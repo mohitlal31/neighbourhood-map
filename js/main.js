1b6d9e14fb8a854713596b649e023ecde3bb78a4
Mohit Lal <mohitlal31@github.com>
2019-02-13 15:14:45 +0530

    Added css styling for the side list view menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var map;
 var marker;
 var bounds;
+var largeInfoWindow;
+var fourSquareData = "";
 
 function initMap()
 {
@@ -11,8 +13,7 @@ function initMap()
 
   bounds = new google.maps.LatLngBounds();
 
-  for(var i=0; i<locationList.length; ++i)
-  {
+  for(var i=0; i<locationList.length; ++i) {
     //create a marker for every location and add it to the 'markers' array
     addMarker(locationList[i]);
     self.markers.push(marker);
@@ -29,7 +30,7 @@ function addMarker(location) {
   });
 
   //display an infoWindow when marker is clicked
-  var largeInfoWindow = new google.maps.InfoWindow();
+  largeInfoWindow = new google.maps.InfoWindow();
   marker.addListener('click', function() {
     populateInfoWindow(this, largeInfoWindow);
   });
@@ -39,16 +40,15 @@ function addMarker(location) {
   });
 }
 
-//adjust the bounds of the map to fit all markers on the screen
+//adjust the bounds of the map to fit all displayed markers on the screen
 function resizeMapBounds() {
+  bounds = new google.maps.LatLngBounds();
   for(var i=0; i<self.markers.length; ++i) {
     if(self.markers[i].getMap() == map) {
-      if(!bounds.contains(self.markers[i].position)) {
         bounds.extend(self.markers[i].position);
-      }
     }
   }
-  map.fitBounds(bounds);
+  map.fitBounds(bounds, 10);
 }
 
 function hideAllMarkers() {
@@ -77,21 +77,27 @@ function filterMarkers(query) {
   }
 }
 
-function populateInfoWindow(marker, infoWindow)
-{
-    //constant '4' is a hack to make the marker bounce twice on click then stop.
-    marker.setAnimation(4);
-    infoWindow.setContent('mohit');
-    infoWindow.open(map, marker);
+function populateInfoWindow(marker, infoWindow) {
+  //constant '4' is a hack to make the marker bounce twice on click then stop.
+  marker.setAnimation(4);
+  getFourSquareData(marker, infoWindow);
 }
 
+function onListItemClicked(location) {
+  var marker = self.markers.find(function(element) {
+    return location.title == element.getTitle();
+  });
+  populateInfoWindow(marker, largeInfoWindow);
+}
 
 
 var ViewModel = function() {
   var self = this;
   this.markers = [];
   this.filterQuery = ko.observable('');
-  //implements a search function to filter the list of locations and markers
+  this.fourSquareClientId = "MQE1HS4YEWHS0XB0W21NWZIL0XHCBMGEJ0QQ4ZPEZ40PB24F";
+  this.fourSquareClientSecret = "YIUDIULYTMBH2LHMGNTULCJOHLTVSBJAVJQ4VE3YLZLVRB15";
+  //implements a search function to filter the list view of locations
   this.filteredLocationList = ko.pureComputed(function() {
     return locationList.filter(function(location) {
       return location.title.toLowerCase().indexOf(self.filterQuery().toLowerCase()) == 0;
@@ -105,3 +111,68 @@ $('#filterQuery').keyup(function() {
   filterMarkers(self.filterQuery());
   resizeMapBounds();
 });
+
+function getFourSquareData(marker, infoWindow) {
+  var coordinates = marker.getPosition().toUrlValue();
+  var venueSearchUrl = "https://api.foursquare.com/v2/venues/search";
+  var parameters = {
+    v: "20181220",
+    ll: coordinates,
+    client_id: self.fourSquareClientId,
+    client_secret: self.fourSquareClientSecret,
+    query: marker.getTitle(),
+    limit: 1
+  };
+
+  $.ajax({
+    dataType: "json",
+    url: venueSearchUrl,
+    data: parameters,
+    success: function(data) {
+      getFourSquarePhotoUrl(marker, infoWindow, data.response.venues[0].id);
+    },
+    error: function(data, textStatus, errorThrown) {
+      onGetFourSquareDataError(marker, infoWindow, data, textStatus, errorThrown);
+    },
+  });
+}
+
+function getFourSquarePhotoUrl(marker, infoWindow, locationId) {
+  var venuePhotosUrl = "https://api.foursquare.com/v2/venues/" + locationId + "/photos";
+  var parameters = {
+    v: "20181220",
+    client_id: self.fourSquareClientId,
+    client_secret: self.fourSquareClientSecret,
+    limit: 1
+  };
+
+  $.ajax({
+    dataType: "json",
+    url: venuePhotosUrl,
+    data: parameters,
+    success: function(data) {
+      var prefix = data.response.photos.items[0].prefix;
+      var suffix = data.response.photos.items[0].suffix;
+      var size = "height200";
+      window.fourSquareData = prefix + size + suffix;
+      onGetFourSquareDataSuccess(marker, infoWindow, window.fourSquareData);
+    },
+    error: function(data, textStatus, errorThrown) {
+      onGetFourSquareDataError(marker, infoWindow, data, textStatus, errorThrown);
+    },
+  });
+}
+
+function onGetFourSquareDataSuccess(marker, infoWindow, photoUrl) {
+  infoWindow.setContent("<div>" + marker.getTitle() + "</div>" +"<img src=" + photoUrl + ">" +
+  "<div>Courtesy: <a target=_blank href=https://foursquare.com> FourSquare</a></div>");
+  infoWindow.open(window.map, marker);
+  //bring the marker to the center of map to display the infoWindow completely
+  window.map.panTo(marker.getPosition());
+}
+
+function onGetFourSquareDataError(marker, infoWindow, data, textStatus, errorThrown) {
+  infoWindow.setContent("<div>Unable to load FourSquare Image</div>" + "<div>textStatus: "
+  + textStatus + "</div>" + "<div>errorThrown: " + errorThrown + "</div>");
+  infoWindow.open(window.map, marker);
+}
--- js/model.js
+++ js/model.js
@@ -1,5 +1,10 @@
 var locationList =
 [
   {title: 'Bannerghatta National Park', location: {lat: 12.800818, lng: 77.577517}},
-  {title: 'Cubbon Park', location: {lat: 12.981095, lng: 77.597057}}
+  {title: 'Cubbon Park', location: {lat: 12.981095, lng: 77.597057}},
+  {title: 'Jaya Prakash Narayana Park', location: {lat: 13.03441, lng: 77.551331}},
+  {title: 'Freedom Park', location: {lat: 12.978271, lng: 77.582158}},
+  {title: 'Sangolli Rayanna Park', location: {lat: 13.007169, lng: 77.557966}},
+  {title: 'Indira Gandhi Musical Fountain Park', location: {lat: 12.985118, lng: 77.591592}},
+  {title: 'Bugle Rock Park', location: {lat: 12.942705, lng: 77.569429}}
 ];
